d030ef4d17e85eeadbcbab4e0bae9044f765cd73
Vellts <zreinx12@gmail.com>
2023-05-19 11:23:34 -0500

    test

--- front/server/index.js
+++ front/server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3002
 
-app.use('/', express.static('public'))
+app.use('*', express.static('dist'))
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
