d894506707fb67cdac30642120a33d49b2e2302c
Vellts <zreinx12@gmail.com>
2023-05-19 10:42:22 -0500

    test

--- apiv1/index.js
+++ apiv1/index.js
@@ -1,8 +1,21 @@
 const express = require('express')
+const cookieParser = require('cookie-parser')
 const app = express()
 const port = 3000
 
+app.use(cookieParser())
+app.use(express.json())
+
 app.get('/', (req, res) => {
+
+    res.cookie('refresh_token', '123456', {
+        httpOnly: true,
+        secure: false,
+        maxAge: 2 * 60 * 1000,
+        // set cookies to all domains and subdomains
+        domain: 'localhost',
+    })
+
     res.json({
         ok: true
     })
